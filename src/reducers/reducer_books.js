e3f9e825dc64ab7e55916dd3a756752e1b26c690
Kin <winterheat@gmail.com>
2017-03-28 07:22:36 -0700

    Add Books Reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+
+export default function() {
+    return [
+        {title: "The Ruby Programming Language", page: 123, author: "David Flanagan"},
+        {title: "Da Vinci Code", page: 456, author: "Dan Brown"},
+        {title: "Smalltalk 80", page: 200, author: "Adele Goldberg "},
+        {title: "Object Oriented JavaScript", page: 300, author: " Stoyan Stefanov"}
+    ]
+}
