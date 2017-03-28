524a6f3af8579a7a23bb2ccb556d73d2b677116d
Kin <winterheat@gmail.com>
2017-03-28 08:26:34 -0700

    change indent level. add how to connect app level state to container comments

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,30 +1,28 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-
+import { connect } from 'react-redux';  // HOW TO CONNECT APP STATE TO CONTAINER
 
 class BookList extends Component {
-    renderBookList() {
-            return this.props.books.map((book) => {
-                return (
-                    <li key={book.title}>{book.title}, {book.author}, {book.numPages} pages</li>
-                )
-            });
-    }
+  renderBookList() {
+    return this.props.books.map((book) => {    // HOW TO CONNECT APP STATE TO CONTAINER: How to use the state
+      return (
+        <li key={book.title}>{book.title}, {book.author}, {book.numPages} pages</li>
+      )
+    });
+  }
 
-    render() {
-        return (
-                <ul>
-                    {this.renderBookList()}
-                </ul>
-        );
-    }
+  render() {
+    return (
+      <ul>
+      {this.renderBookList()}
+      </ul>
+    );
+  }
 }
 
-function mapStateToProps(state) {
-
-    return {
-        books: state.books
-    }
+function mapStateToProps(state) {    // HOW TO CONNECT APP STATE TO CONTAINER
+  return {
+    books: state.books
+  }
 }
 
-export default connect(mapStateToProps)(BookList);
+export default connect(mapStateToProps)(BookList);    // HOW TO CONNECT APP STATE TO CONTAINER
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 
 export default function() {
-    return [
-        {title: "The Ruby Programming Language", numPages: 123, author: "David Flanagan"},
-        {title: "Da Vinci Code", numPages: 456, author: "Dan Brown"},
-        {title: "Smalltalk 80", numPages: 200, author: "Adele Goldberg "},
-        {title: "Object Oriented JavaScript", numPages: 300, author: " Stoyan Stefanov"}
-    ]
+  return [
+    {title: "The Ruby Programming Language", numPages: 123, author: "David Flanagan"},
+    {title: "Da Vinci Code", numPages: 456, author: "Dan Brown"},
+    {title: "Smalltalk 80", numPages: 200, author: "Adele Goldberg "},
+    {title: "Object Oriented JavaScript", numPages: 300, author: " Stoyan Stefanov"}
+  ]
 }
