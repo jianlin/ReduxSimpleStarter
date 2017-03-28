c9258bd5ad2dee74542dbb7a65effc0ca6e67556
Kin <winterheat@gmail.com>
2017-03-28 10:25:32 -0700

    ok

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';  // HOW TO CONNECT APP STATE TO CONTAINER
+import { connect } from 'react-redux';  // HOW TO CONNECT APP STATE TO CONTAINER: need the connect from the library React-Redux
 
 class BookList extends Component {
   renderBookList() {
-    return this.props.books.map((book) => {    // HOW TO CONNECT APP STATE TO CONTAINER: How to use the state
+    return this.props.books.map((book) => {    // HOW TO CONNECT APP STATE TO CONTAINER: How to use the state: as props
       return (
         <li key={book.title}>{book.title}, {book.author}, {book.numPages} pages</li>
       )
@@ -19,10 +19,10 @@ class BookList extends Component {
   }
 }
 
-function mapStateToProps(state) {    // HOW TO CONNECT APP STATE TO CONTAINER
+function mapStateToProps(state) {    // HOW TO CONNECT APP STATE TO CONTAINER: map the app level state to props
   return {
     books: state.books
   }
 }
 
-export default connect(mapStateToProps)(BookList);    // HOW TO CONNECT APP STATE TO CONTAINER
+export default connect(mapStateToProps)(BookList);    // HOW TO CONNECT APP STATE TO CONTAINER: export this smart component, AKA container
